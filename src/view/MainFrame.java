6c83017ba87154d0ec23e122ae2aae78cbde4c8e
emilime93 <emil93@gmail.com>
2017-12-07 13:24:30 +0100

    Created basic input and output text areas.

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -8,11 +8,11 @@ public class MainFrame extends JFrame {
     private final int WINDOW_WIDTH = 700;
     private final int WINDOW_HEIGHT = 500;
 
-    private JPanel mainPanel = new JPanel();
-    private JTextArea inputArea = new JTextArea();
-    private JTextArea resultArea = new JTextArea();
+    private JPanel mainPanel;
+    private JTextArea inputArea;
+    private JTextArea resultArea;
 
-    private JButton runQueryButton = new JButton("Run");
+    private JButton runQueryButton;
 
     private GroupLayout layoutManager;
 
@@ -27,21 +27,41 @@ public class MainFrame extends JFrame {
     }
 
     private void finalizeFrame() {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        pack();
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
     }
 
+    /**
+     * Initializes the frame. Such as adding components, their listeners and setting the layout.
+     */
     private void initializeFrame() {
-        layoutManager = new GroupLayout(mainPanel);
-        mainPanel.setLayout(layoutManager);
-
+        mainPanel = new JPanel(new GridBagLayout());
         getContentPane().add(mainPanel);
 
-//        mainPanel.add(runQueryButton);
-        
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+
+        inputArea = new JTextArea(10, 40);
+        inputArea.setBackground(new Color(150,150,150));
+        JScrollPane scrollPane = new JScrollPane(inputArea);
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Input:"));
+        mainPanel.add(scrollPane, constraints);
+        constraints.gridy++;
+
+        resultArea = new JTextArea(10, 40);
+        resultArea.setEditable(false);
+        JScrollPane resultScrollPane = new JScrollPane(resultArea);
+        resultScrollPane.setBorder(BorderFactory.createTitledBorder("Results:"));
+        mainPanel.add(resultScrollPane, constraints);
+        constraints.gridy++;
+
+        runQueryButton = new JButton("Run");
+        mainPanel.add(runQueryButton, constraints);
     }
 
 
