c3c1a179ac5050030ebe23077901456a9d24e570
emilime93 <emil93@gmail.com>
2017-12-07 14:01:51 +0100

    Added a basic menu and a basic structure for getProducts (get all products). Read TODO's.

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Product {
+
+    //TODO DO THIS GUISE
+
+    @Override
+    public String toString() {
+        return "Change me: Product";
+    }
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,24 +1,33 @@
 package view;
 
+import controller.Controller;
+import model.Product;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MainFrame extends JFrame {
+
+    // TODO Set the controller before I create the menu, otherwise null-pointer
 
     private final int WINDOW_WIDTH = 700;
     private final int WINDOW_HEIGHT = 500;
 
+    // The menu
+    private JMenuBar menuBar;
+    private JMenu mainMenu;
+
+    // All panels
     private JPanel mainPanel;
     private JTextArea inputArea;
     private JTextArea resultArea;
 
     private JButton runQueryButton;
 
-    private GroupLayout layoutManager;
-
-    public MainFrame() {
-        super();
-    }
+    private Controller controller;
 
     public MainFrame(String frameName) {
         super(frameName);
@@ -39,6 +48,10 @@ public class MainFrame extends JFrame {
      * Initializes the frame. Such as adding components, their listeners and setting the layout.
      */
     private void initializeFrame() {
+        // Create the menu
+        createMenu();
+        setJMenuBar(menuBar);
+
         mainPanel = new JPanel(new GridBagLayout());
         getContentPane().add(mainPanel);
 
@@ -46,13 +59,14 @@ public class MainFrame extends JFrame {
         constraints.gridx = 0;
         constraints.gridy = 0;
 
+        // Input field
         inputArea = new JTextArea(10, 40);
-        inputArea.setBackground(new Color(150,150,150));
         JScrollPane scrollPane = new JScrollPane(inputArea);
         scrollPane.setBorder(BorderFactory.createTitledBorder("Input:"));
         mainPanel.add(scrollPane, constraints);
         constraints.gridy++;
 
+        // Output field
         resultArea = new JTextArea(10, 40);
         resultArea.setEditable(false);
         JScrollPane resultScrollPane = new JScrollPane(resultArea);
@@ -60,9 +74,72 @@ public class MainFrame extends JFrame {
         mainPanel.add(resultScrollPane, constraints);
         constraints.gridy++;
 
+        // Run button
         runQueryButton = new JButton("Run");
+        runQueryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         mainPanel.add(runQueryButton, constraints);
     }
 
+    private void createMenu() {
+        menuBar = new JMenuBar();
 
+        mainMenu = new JMenu("Action");
+        menuBar.add(mainMenu);
+
+        JMenuItem menuItem1 = new JMenuItem("Hämta alla produkter");
+        menuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO enable this when null pointer is solved
+//                displayProducts(controller.getProducts());
+                inputArea.append(e.getActionCommand());
+            }
+        });
+        mainMenu.add(menuItem1);
+
+        JMenuItem menuItem2 = new JMenuItem("Item 2");
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputArea.append(e.getActionCommand());
+            }
+        });
+        mainMenu.add(menuItem2);
+
+        JMenuItem menuItem3 = new JMenuItem("Item 3");
+        menuItem3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputArea.append(e.getActionCommand());
+            }
+        });
+        mainMenu.add(menuItem3);
+
+        JMenuItem menuItem4 = new JMenuItem("Item 4");
+        menuItem4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inputArea.append(e.getActionCommand());
+            }
+        });
+        mainMenu.add(menuItem4);
+
+    }
+
+    private void displayProducts(ArrayList<Product> productList) {
+        for (Product p :
+                productList) {
+            this.resultArea.append(p.toString());
+        }
+        // TODO display the products
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
 }
